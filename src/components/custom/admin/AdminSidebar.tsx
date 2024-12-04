39e86a25184293ac6e4c012d82edbe6a68364bac
Cristian <cj83774932@gmail.com>
2024-12-04 11:13:53 -0600

    Added "Advertisement" as a new item in Sidebar, and SingleView form (design-wise)

--- src/components/custom/admin/AdminSidebar.tsx
+++ src/components/custom/admin/AdminSidebar.tsx
@@ -5,7 +5,7 @@ import {
   CommandList,
 } from "@/components/ui/command"
 
-import { NotepadText, UserRound } from "lucide-react"
+import { NotepadText, UserRound, Megaphone } from "lucide-react"
 import { Link } from "react-router-dom"
 
 import {
@@ -17,32 +17,36 @@ import {
 
 export function AdminSidebar() {
   return(
-    <>
-    <Command className="bg-card rounded-none h-full">
-      <CommandList className="overflow-y-auto">
-        <CommandGroup heading="Actions">
-          <CommandItem>
-          <UserRound className="mr-2"/>
-            <Link to="/admin/dashboard/egresados">Egresado</Link>
-          </CommandItem>
-          <CommandItem>
-            <NotepadText className="mr-2"/>
-            <Link to="/admin/dashboard/forms">
-              <ContextMenu>
-                <ContextMenuTrigger>Forms</ContextMenuTrigger>
-                <ContextMenuContent>
-                  <ContextMenuItem>
-                    <Link to="/">
-                      Create
-                    </Link>
-                  </ContextMenuItem>
-                </ContextMenuContent>
-              </ContextMenu>
-            </Link>
-          </CommandItem>
-        </CommandGroup>
-      </CommandList>
-    </Command>
-    </>
+      <>
+        <Command className="bg-card rounded-none h-full">
+          <CommandList className="overflow-y-auto">
+            <CommandGroup heading="Actions">
+              <CommandItem>
+                <UserRound className="mr-2"/>
+                <Link to="/admin/dashboard/egresados">Egresado</Link>
+              </CommandItem>
+              <CommandItem>
+                <NotepadText className="mr-2"/>
+                <Link to="/admin/dashboard/forms">
+                  <ContextMenu>
+                    <ContextMenuTrigger>Forms</ContextMenuTrigger>
+                    <ContextMenuContent>
+                      <ContextMenuItem>
+                        <Link to="/">
+                          Create
+                        </Link>
+                      </ContextMenuItem>
+                    </ContextMenuContent>
+                  </ContextMenu>
+                </Link>
+              </CommandItem>
+              <CommandItem>
+                <Megaphone className="mr-2" />
+                <Link to="/admin/dashboard/advertisements">Advertisement</Link>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </>
   )
 }
